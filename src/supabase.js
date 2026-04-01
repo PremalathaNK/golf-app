f4c174e172f664d022835c89a333550a7926d0c1
PremalathaNK <nkreddypremalatha@gmail.com>
2026-04-01 12:52:23 +0530

    Use env vars for Supabase keys and ignore .env

--- src/supabase.js
+++ src/supabase.js
@@ -7,7 +7,6 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://ugnmpirtjxvfxvscthxd.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnbm1waXJ0anh2Znh2c2N0aHhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMTQ0MjksImV4cCI6MjA5MDU5MDQyOX0.ddSLkKD3z6Xa-u7i73yn3iaNBboifV18BZXxnkfJC2M"
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey =  import.meta.env.VITE_SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
